e2537420485a26b4f5011db2a118b187bad183b2
daichi <bumpk1002@gmail.com>
2019-05-15 17:04:26 +0900

    [Finish]5-3

--- Gemfile
+++ Gemfile
@@ -66,3 +66,7 @@ gem 'devise'
 
 
 gem 'device'
+
+gem "refile", require: "refile/rails", github: 'manfe/refile'
+
+gem "refile-mini_magick"
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ab7adcc4a36944c381d6cbdd44780998d2b04f8eeb9dbc3069c6e8f14a3d0243560fb0e9a36386da8eecdabcb56596b1cd2806c6a8555b882b2da703325fed98'
